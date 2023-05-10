91a35f5851b5339cb843c107892b1a4348c3746e
Johan Morganti <me@johan.sh>
2023-05-10 23:24:25 +0900

    delete backups

--- tests/website_2/~domeo/Exemples-Motif/motifgif/patchlevel.h
+++
@@ -1,9 +0,0 @@
-/* 
- * (c) Copyright 1989, 1990, 1991, 1992 OPEN SOFTWARE FOUNDATION, INC. 
- * ALL RIGHTS RESERVED 
-*/ 
-/* 
- * Motif Release 1.2
-*/ 
-/*   $RCSfile: patchlevel.h,v $ $Revision: 1.3 $ $Date: 92/03/13 15:39:39 $ */
-#define PATCHLEVEL 2
